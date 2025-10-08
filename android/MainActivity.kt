1cdd9c02ee6aad513d80ffee26c2c3c23e105ac3
ns7295754-tech <ns7295754@gmail.com>
2025-10-09 01:14:05 +0530

    Delete android directory

--- android/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.bigfun.voicechat   // <- replace with your actual package name if different
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
